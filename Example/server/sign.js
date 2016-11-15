4a74a64a52cec98dad49fc9a99c1a9cfcba38ce4
lavystord <lavystord@gmail.com>
2016-11-15 16:19:33 +0800

    refactor signString

--- Example/server/sign.js
+++ Example/server/sign.js
@@ -4,37 +4,44 @@
 // const keys = require('lodash/keys');
 // const assign = require('lodash/assign');
 const crypto = require('crypto');
+const privateKey = require("./privateKey");
 
 const bizContent = {
     "timeout_express": "30m",
     "product_code": "QUICK_MSECURITY_PAY",
-    "total_amount": "0.01",
-    "subject": "12",
+    "subject": "我是商品名称",
     "body": "我是测试数据",
-    "out_trade_no": "707665424",
+    // "out_trade_no": "707665424",
     // "total_amount": "10.0",
     // "seller_id": "2088102147948060",
 };
 
 
-const constOrder = {
-    app_id: "aaa",
-    timestamp: "bbb",
-    biz_content: "ccc",
+//支付宝所需要的常量参数
+const payConfig = {
+    app_id: privateKey.app_id, //请替换为自己的appId
     method: "alipay.trade.app.pay",
     charset: "utf-8",
     version: "1.0",
     format: "JSON",
-    notify_url: "http://www.martian-life.com:3001/api/1.0/gateway/alipay/receiveNotify",
+    notify_url: "http://www.martian-life.com:3001/api/1.0/gateway/alipay/receiveNotify", //请替换为自己的回调接口
     sign_type: "RSA",
 };
 
-const privateKey = require("./privateKey");
+function forDec(number) {
+    return number >9 ? number : `0${number}`
+}
 
 function generateOutTradeNo ()  {
     return Date.now().toString();
 }
 
+function genBizContent(totalAmount, payId) {
+    return Object.assign({}, bizContent, {
+        total_amount: totalAmount,
+        out_trade_no: payId,
+    });
+}
 /**
  * 生成订单字符串
  * 本逻辑在生产环境中应该被部署在服务器端
@@ -43,17 +50,23 @@ function generateOutTradeNo ()  {
  */
 function signOrderString(totalAmount) {
     let stringToSign = "";
-    const bizContent2 = Object.assign({}, bizContent, {
-        total_amount: totalAmount,
-        out_trade_no: generateOutTradeNo(),
-    })
+
+
+    // 填写你定义的订单总额和订单号, 这里订单号为任意字符串,生产环境请根据自己业务逻辑替换
+    const bizContent = genBizContent(totalAmount, generateOutTradeNo());
+
+
     const now = new Date();
-    const timestamp = '2016-07-29 16:55:53';
-    const constOrder2 = Object.assign({}, constOrder, {
-        biz_content: JSON.stringify(bizContent2),
-        app_id: privateKey.app_id,
+    const timestamp=`${now.getFullYear()}-${forDec(now.getMonth()+1)}-${forDec(now.getDate())} ${forDec(now.getHours())}:${forDec(now.getMinutes())}:${forDec(now.getSeconds())}` ;
+    // console.log(result);
+    // const now = new Date();
+
+    const constOrder2 = Object.assign({}, payConfig, {
+        biz_content: JSON.stringify(bizContent),
+        // app_id: privateKey.app_id,
         timestamp,
     });
+
     const constOrderArray = Object.keys(constOrder2).sort();
     constOrderArray.forEach((ele, idx) => {
         if(idx > 0) {
