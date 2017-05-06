d7f42b4b571bdb9b2eafe42cd5c6f6acc1f530eb
lavystord <lavystord@gmail.com>
2017-05-06 19:38:30 +0800

    upgrade 0.40+

--- ios/RNAlipay/MartianRnAlipay.h
+++ ios/RNAlipay/MartianRnAlipay.h
@@ -1,9 +1,21 @@
+#if __has_include(<React/RCTUtils.h>)
+#import <React/RCTLog.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTConvert.h>
+#import <React/RCTBridge.h>
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTRootView.h>
+#import <React/RCTUtils.h>
+#else // back compatibility for RN version < 0.40
 #import "RCTLog.h"
+#import "RCTBridgeModule.h"
 #import "RCTConvert.h"
+#import "RCTEventDispatcher.h"
+#import "RCTRootView.h"
 #import "RCTBridge.h"
 #import "RCTUtils.h"
-#import "RCTEventDispatcher.h"
-#import "RCTBridgeModule.h"
+#endif
+
 #import <UIKit/UIKit.h>
 
 @interface MartianRnAlipay : NSObject <RCTBridgeModule>
